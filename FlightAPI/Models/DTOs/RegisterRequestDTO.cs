98cb29d34f296ce79833dc1605b688946f5cec26
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-04-16 23:52:28 +0200

    Add RegisterRequest DTO

---
+++ FlightAPI/Models/DTOs/RegisterRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace FlightAPI.Models.DTOs
+{
+    public class RegisterRequestDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
