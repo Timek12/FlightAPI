57fc5d3b07e991f7e7c562056480c2aee9d05641
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-04-16 23:27:46 +0200

    Add identity services and db context atributes, create constructor

---
+++ FlightAPI/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using FlightAPI.Data;
+using FlightAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightAPI.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private ApiResponse _response;
+
+        public AuthController(ApplicationDbContext db, string secretKey, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _response = new ApiResponse();
+        }
+    }
+}
