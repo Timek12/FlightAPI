579043f592088c784e611dd61b66ff56ddf2033f
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-04-16 23:23:13 +0200

    Add ApiResponse model

---
+++ FlightAPI/Models/ApiResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.Net;
+
+namespace FlightAPI.Models
+{
+    public class ApiResponse
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public bool IsSuccess { get; set; } = false;
+        public List<string> Errors { get; set; } = [];
+        public object Result { get; set; }
+    }
+}
