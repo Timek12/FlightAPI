74ba775d456c07afad89f42fac9accced9766d61
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-04-17 11:36:51 +0200

    Add constats file

---
+++ FlightAPI/Utility/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace FlightAPI.Utility
+{
+    public class Constants
+    {
+        public const string Role_Admin = "admin";
+        public const string Role_Customer = "customer";
+    }
+}
