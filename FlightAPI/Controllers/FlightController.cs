5654fe8bf964b047efcb425e13fd40ddc39247cc
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-04-19 00:40:59 +0200

    Refactor flight controller to use flight service layer

--- FlightAPI/Controllers/FlightController.cs
+++ FlightAPI/Controllers/FlightController.cs
@@ -1,157 +1,152 @@
-﻿using AutoMapper;
-using AutoMapper.QueryableExtensions;
-using FlightAPI.Data;
-using FlightAPI.Models;
+﻿using FlightAPI.Models;
 using FlightAPI.Models.DTOs;
-using FlightAPI.Utility.Enums;
+using FlightAPI.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using System.Net;
 
 namespace FlightAPI.Controllers
 {
     [Route("api/flights")]
     [ApiController]
-    public class FlightController : ControllerBase
+    public class FlightController(IFlightService flightService) : ControllerBase
     {
-        private readonly ApplicationDbContext _db;
-        private readonly IMapper _mapper;
-        private ApiResponse _response;
-
-        public FlightController(ApplicationDbContext db, IMapper mapper)
-        {
-            _db = db;
-            _mapper = mapper;
-            _response = new ApiResponse();
-        }
+        private readonly IFlightService _flightService = flightService;
+        private readonly ApiResponse _response = new();
 
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
-            _response.Result = await _db.Flights
-                .Include(f => f.Plane)
-                .ProjectTo<FlightDTO>(_mapper.ConfigurationProvider)
-                .ToListAsync();
-
-            _response.StatusCode = HttpStatusCode.OK;
-            _response.IsSuccess = true;
-            return Ok(_response);
+            try
+            {
+                _response.Result = await _flightService.GetAllFlights();
+                _response.StatusCode = HttpStatusCode.OK;
+                _response.IsSuccess = true;
+                return Ok(_response);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                _response.StatusCode = HttpStatusCode.NotFound;
+                _response.Errors.Add(ex.Message);
+                _response.IsSuccess = false;
+                return NotFound(_response);
+            }
         }
 
         [HttpGet("{id:int}")]
         public async Task<IActionResult> Get(int id)
         {
-            if (id <= 0)
+            try
+            {
+                _response.Result = await _flightService.GetFlightDTOById(id); ;
+                _response.IsSuccess = true;
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
+            }
+            catch (ArgumentException ex)
             {
                 _response.StatusCode = HttpStatusCode.BadRequest;
-                _response.Errors.Add("Incorrect flight id");
+                _response.Errors.Add(ex.Message);
                 _response.IsSuccess = false;
                 return BadRequest(_response);
             }
-
-            FlightDTO? flightDTO = await _db.Flights
-                .Include(f => f.Plane)
-                .ProjectTo<FlightDTO>(_mapper.ConfigurationProvider)
-                .FirstOrDefaultAsync(f => f.Id == id);
-
-            if (flightDTO is null)
+            catch (KeyNotFoundException ex)
             {
-                _response.StatusCode = HttpStatusCode.BadRequest;
-                _response.Errors.Add("Flight with given id does not exists");
+                _response.StatusCode = HttpStatusCode.NotFound;
+                _response.Errors.Add(ex.Message);
                 _response.IsSuccess = false;
                 return NotFound(_response);
             }
-
-            _response.Result = flightDTO;
-            _response.IsSuccess = true;
-            _response.StatusCode = HttpStatusCode.OK;
-            return Ok(_response);
         }
 
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] CreateFlightDTO flightDTO)
         {
-            if (flightDTO is null || flightDTO.PlaneId <= 0)
+            try
+            {
+                var newFlightDTO = await _flightService.CreateFlight(flightDTO);
+                _response.Result = newFlightDTO;
+                _response.IsSuccess = true;
+                _response.StatusCode = HttpStatusCode.Created;
+                return CreatedAtAction(nameof(Get), new { id = newFlightDTO.Id }, _response);
+            }
+            catch (ArgumentNullException ex)
             {
                 _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.Errors.Add(ex.Message);
                 _response.IsSuccess = false;
-                _response.Errors.Add("Invalid flight data");
                 return BadRequest(_response);
             }
-
-            var plane = await _db.Planes.FindAsync(flightDTO.PlaneId);
-            if (plane is null)
+            catch (ArgumentException ex)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.Errors.Add(ex.Message);
+                _response.IsSuccess = false;
+                return BadRequest(_response);
+            }
+            catch (KeyNotFoundException ex)
             {
                 _response.StatusCode = HttpStatusCode.NotFound;
+                _response.Errors.Add(ex.Message);
                 _response.IsSuccess = false;
-                _response.Errors.Add("Plane with given id does not exists");
                 return NotFound(_response);
             }
-
-            Flight newFlight = new()
-            {
-                FlightNumber = flightDTO.FlightNumber,
-                DepartureDate = flightDTO.DepartureDate,
-                ArrivalLocation = flightDTO.ArrivalLocation,
-                DepartureLocation = flightDTO.DepartureLocation,
-                PlaneId = plane.Id
-            };
-
-            _db.Flights.Add(newFlight);
-            await _db.SaveChangesAsync();
-
-            _response.Result = _mapper.Map<FlightDTO>(newFlight);
-            _response.IsSuccess = true;
-            _response.StatusCode = HttpStatusCode.Created;
-            return CreatedAtAction(nameof(Get), new { id = newFlight.Id }, _response);
         }
 
-        [HttpPut]
-        public async Task<IActionResult> Update(UpdateFlightDTO flightDTO)
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Update(int id, UpdateFlightDTO flightDTO)
         {
-            var flightFromDb = await _db.Flights.FindAsync(flightDTO.Id);
-            if (flightFromDb is null)
+            try
+            {
+                _response.Result = await _flightService.UpdateFlight(id, flightDTO);
+                _response.IsSuccess = true;
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
+            }
+            catch (ArgumentNullException ex)
             {
                 _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.Errors.Add(ex.Message);
                 _response.IsSuccess = false;
-                _response.Errors.Add("Invalid flight data");
                 return BadRequest(_response);
             }
-
-            _mapper.Map(flightDTO, flightFromDb);
-
-            await _db.SaveChangesAsync();
-
-            _response.Result = _mapper.Map<FlightDTO>(flightFromDb);
-            _response.IsSuccess = true;
-            _response.StatusCode = HttpStatusCode.OK;
-            return Ok(_response);
+            catch (ArgumentException ex)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.Errors.Add(ex.Message);
+                _response.IsSuccess = false;
+                return BadRequest(_response);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                _response.StatusCode = HttpStatusCode.NotFound;
+                _response.Errors.Add(ex.Message);
+                _response.IsSuccess = false;
+                return NotFound(_response);
+            }
         }
 
         [HttpDelete("{id:int}")]
         public async Task<IActionResult> Delete(int id)
         {
-            if (id <= 0)
+            try
+            {
+                await _flightService.Delete(id);
+                return NoContent();
+            }
+            catch (ArgumentException ex)
             {
                 _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.Errors.Add(ex.Message);
                 _response.IsSuccess = false;
-                _response.Errors.Add("Invalid flight id");
                 return BadRequest(_response);
             }
-
-            var flightFromDb = await _db.Flights.FindAsync(id);
-            if (flightFromDb is null)
+            catch (KeyNotFoundException ex)
             {
-                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.StatusCode = HttpStatusCode.NotFound;
+                _response.Errors.Add(ex.Message);
                 _response.IsSuccess = false;
-                _response.Errors.Add("Flight with given id does not exists");
                 return NotFound(_response);
             }
-
-            _db.Flights.Remove(flightFromDb);
-            await _db.SaveChangesAsync();
-
-            return NoContent();
         }
     }
 }
