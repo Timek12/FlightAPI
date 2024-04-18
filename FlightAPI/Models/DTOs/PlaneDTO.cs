a1af3eadf523b71f569a107bdf9a763dbad7994a
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-04-18 19:35:48 +0200

    Create PlaneDTO

---
+++ FlightAPI/Models/DTOs/PlaneDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlightAPI.Models.DTOs
+{
+    public class PlaneDTO
+    {
+        public string Model { get; set; }
+        public string Type { get; set; }
+    }
+}
