0071a7118a85dbf02f0b31b499f07c112641adf6
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-04-18 13:38:08 +0200

    Create Flight DTO

---
+++ FlightAPI/Models/DTOs/FlightDTO.cs
@@ -0,0 +1,14 @@
+﻿using FlightAPI.Utility.Enums;
+
+namespace FlightAPI.Models.DTOs
+{
+    public class FlightDTO
+    {
+        public int Id { get; set; }
+        public string FlightNumber { get; set; }
+        public DateTime? DepartureDate { get; set; }
+        public string ArrivalLocation { get; set; }
+        public string DepartureLocation { get; set; }
+        public string PlaneType { get; set; }
+    }
+}
