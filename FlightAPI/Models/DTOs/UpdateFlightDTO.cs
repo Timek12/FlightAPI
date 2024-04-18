16a40009219f7b58f7e1189da20e0285ba3771ba
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-04-18 20:45:41 +0200

    Create UpdateFlightDTO

---
+++ FlightAPI/Models/DTOs/UpdateFlightDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FlightAPI.Models.DTOs
+{
+    public class UpdateFlightDTO
+    {
+        public int Id { get; set; }
+        public string FlightNumber { get; set; }
+        public DateTime? DepartureDate { get; set; }
+        public string ArrivalLocation { get; set; }
+        public string DepartureLocation { get; set; }
+        public int PlaneId { get; set; }
+    }
+}
