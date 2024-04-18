482ec40f9e8292e5f46097ef6e1cb9058570f9e5
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-04-18 16:12:57 +0200

    Add Planes table, seed planes table with sample data

--- FlightAPI/Data/ApplicationDbContext.cs
+++ FlightAPI/Data/ApplicationDbContext.cs
@@ -11,10 +11,53 @@ namespace FlightAPI.Data
 
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<Flight> Flights { get; set; }
+        public DbSet<Plane> Planes { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+            builder.Entity<Plane>().HasData(
+                new Plane
+                {
+                    Id = 1,
+                    Model = "A380",
+                    Type = "Airbus",
+                    Manufacturer = "Airbus",
+                    Capacity = 853,
+                    CruiseSpeed = 903,
+                    Range = 15700
+                },
+                new Plane
+                {
+                    Id = 2,
+                    Model = "747",
+                    Type = "Boeing",
+                    Manufacturer = "Boeing",
+                    Capacity = 660,
+                    CruiseSpeed = 920,
+                    Range = 13450
+                },
+                new Plane
+                {
+                    Id = 3,
+                    Model = "E195",
+                    Type = "Embraer",
+                    Manufacturer = "Embraer",
+                    Capacity = 132,
+                    CruiseSpeed = 870,
+                    Range = 3700
+                },
+                new Plane
+                {
+                    Id = 4,
+                    Model = "CRJ1000",
+                    Type = "Bombardier",
+                    Manufacturer = "Bombardier",
+                    Capacity = 104,
+                    CruiseSpeed = 829,
+                    Range = 2752
+                }
+            );
 
             builder.Entity<Flight>().HasData(
                 new Flight
@@ -24,7 +67,7 @@ namespace FlightAPI.Data
                     DepartureDate = new DateTime(2024, 10, 15, 12, 30, 0),
                     DepartureLocation = "Warsaw",
                     ArrivalLocation = "New York",
-                    PlaneType = PlaneType.Boeing
+                    PlaneId = 2
                 },
                 new Flight
                 {
@@ -33,7 +76,7 @@ namespace FlightAPI.Data
                     DepartureDate = new DateTime(2024, 10, 15, 12, 30, 0),
                     DepartureLocation = "Warsaw",
                     ArrivalLocation = "Tokio",
-                    PlaneType = PlaneType.Embraer
+                    PlaneId = 3 
                 },
                 new Flight
                 {
@@ -42,7 +85,7 @@ namespace FlightAPI.Data
                     DepartureDate = new DateTime(2024, 10, 15, 12, 30, 0),
                     DepartureLocation = "Warsaw",
                     ArrivalLocation = "Phuket",
-                    PlaneType = PlaneType.Airbus
+                    PlaneId = 1
                 }
             );
         }
