8748fb1f74810587f69d1f473b62b5126113eaef
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-04-18 16:06:01 +0200

    Create Plane model

---
+++ FlightAPI/Models/Plane.cs
@@ -0,0 +1,13 @@
+﻿namespace FlightAPI.Models
+{
+    public class Plane
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public string Type { get; set; }
+        public string Manufacturer { get; set; }
+        public int Capacity { get; set; }
+        public double CruiseSpeed { get; set; }
+        public double Range { get; set; }
+    }
+}
