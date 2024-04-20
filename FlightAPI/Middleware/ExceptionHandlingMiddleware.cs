d9b3a5c88f4d5af7691231b6d16d062d4cd15797
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-04-20 19:17:00 +0200

    Add exception handling middleware

---
+++ FlightAPI/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,68 @@
+﻿using FlightAPI.Exceptions;
+using FlightAPI.Models;
+using Newtonsoft.Json;
+using System;
+using System.Net;
+
+namespace FlightAPI.Middleware
+{
+    public class ExceptionHandlingMiddleware
+    {
+        public readonly RequestDelegate _next;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var response = new ApiResponse()
+            {
+                StatusCode = GetStatusCode(exception),
+                Errors = [exception.Message],
+                IsSuccess = false
+            };
+
+            var result = JsonConvert.SerializeObject(response);
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)response.StatusCode;
+
+            return context.Response.WriteAsync(result);
+        }
+
+        private static HttpStatusCode GetStatusCode(Exception exception)
+        {
+            switch (exception)
+            {
+                case UserNotFoundException:
+                case FlightNotFoundException:
+                    return HttpStatusCode.NotFound;
+                case AuthenticationException:
+                    return HttpStatusCode.Unauthorized;
+                case FailedToCreateUserException:
+                case FailedToGenerateTokenException:
+                case NullFlightDataException:
+                case InvalidFlightIdException:
+                case InvalidPlaneIdException:
+                case InvalidFlightDataException:
+                    return HttpStatusCode.BadRequest;
+                default:
+                    return HttpStatusCode.InternalServerError;
+            }
+        }
+    }
+}
