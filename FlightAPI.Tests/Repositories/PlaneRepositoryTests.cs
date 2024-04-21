900de5a3a07b23bfbd72e33db46668b2a35ceb10
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-04-21 19:01:22 +0200

    Add unit tests for PlaneRepository

---
+++ FlightAPI.Tests/Repositories/PlaneRepositoryTests.cs
@@ -0,0 +1,54 @@
+﻿using Moq;
+using Microsoft.EntityFrameworkCore;
+using FlightAPI.Repositories.Implementations;
+using FlightAPI.Models;
+using FlightAPI.Data;
+
+namespace FlightAPI.Tests.Repositories
+{
+    public class PlaneRepositoryTests
+    {
+        private readonly PlaneRepository _planeRepository;
+        private readonly Mock<DbSet<Plane>> _mockSet;
+        private readonly Mock<IApplicationDbContext> _mockContext;
+
+        public PlaneRepositoryTests()
+        {
+            _mockContext = new Mock<IApplicationDbContext>();
+            _mockSet = new Mock<DbSet<Plane>>();
+            _mockContext.Setup(p => p.Planes).Returns(_mockSet.Object);
+            _planeRepository = new PlaneRepository(_mockContext.Object);
+        }
+
+        [Fact]
+        public async Task GetPlaneById_PlaneExists_ReturnsCorrectPlane()
+        {
+            // Arrange
+            var id = 1;
+            var plane = new Plane { Id = id };
+            _mockSet.Setup(p => p.FindAsync(id)).ReturnsAsync( plane );
+
+            // Act
+            var result = await _planeRepository.GetPlaneById(id);
+
+            // Assert
+            Assert.Equal(plane, result);
+            _mockSet.Verify(p => p.FindAsync(id), Times.Once());
+        }
+
+        [Fact]
+        public async Task GetPlaneById_PlaneDoesNotExist_ReturnsNull()
+        {
+            // Arrane
+            var id = 1;
+            _mockSet.Setup(p => p.FindAsync(id)).ReturnsAsync((Plane)null);
+
+            // Act
+            var result = await _planeRepository.GetPlaneById(id);
+
+            // Assert
+            Assert.Null(result);
+            _mockSet.Verify(p => p.FindAsync(id), Times.Once());
+        }
+    }
+}
