2d06b4521ab0245413fad1b27b51cec6f7a368d5
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-04-22 21:52:49 +0200

    Refactor dependencies initialization for AuthService unit tests, add more verification checks in LoginUser_ValidCredentials_ReturnsLoginResponseDTO

--- FlightAPI.Tests/Services/AuthServiceTests.cs
+++ FlightAPI.Tests/Services/AuthServiceTests.cs
@@ -4,6 +4,7 @@ using FlightAPI.Models.DTOs;
 using FlightAPI.Repositories.Interfaces;
 using FlightAPI.Services.Implementations;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Moq;
@@ -29,12 +30,12 @@ namespace FlightAPI.Tests.Services
                 userStoreMock.Object, null, null, null, null, null, null, null, null);
 
             var mockSecretKeySection = new Mock<IConfigurationSection>();
-            mockSecretKeySection.SetupGet(m => m.Value).Returns("rKOAaacAwIJBrebML2DWZwmQTEBzbYRf");
-            _mockConfiguration.Setup(a => a.GetSection("ApiSettings:SecretKey")).Returns(mockSecretKeySection.Object);
+            mockSecretKeySection.SetupGet(m => m.Value).Returns("JWTPasswordShouldBeSecurelyStoredInServiceSpecificDesign");
+            _mockConfiguration.Setup(a => a.GetSection("JwtSettings:Secret")).Returns(mockSecretKeySection.Object);
 
             var mockTokenExpirationDaysSection = new Mock<IConfigurationSection>();
-            mockTokenExpirationDaysSection.SetupGet(m => m.Value).Returns("1");
-            _mockConfiguration.Setup(a => a.GetSection("ApiSettings:TokenExpirationDays")).Returns(mockTokenExpirationDaysSection.Object);
+            mockTokenExpirationDaysSection.SetupGet(m => m.Value).Returns("7");
+            _mockConfiguration.Setup(a => a.GetSection("JwtSettings:TokenExpirationDays")).Returns(mockTokenExpirationDaysSection.Object);
 
             _authService = new AuthService(
                 _mockUserRepository.Object,
@@ -55,7 +56,8 @@ namespace FlightAPI.Tests.Services
                 LastName = "User",
                 Id = Guid.NewGuid().ToString()
             };
-            _mockUserRepository.Setup(x => x.GetUserByEmail(loginRequest.Email)).ReturnsAsync(user);
+
+            _mockUserManager.Setup(x => x.FindByEmailAsync(loginRequest.Email)).ReturnsAsync(user);
             _mockUserManager.Setup(x => x.CheckPasswordAsync(user, loginRequest.Password)).ReturnsAsync(true);
             var roles = new List<string> { "Admin" };
             _mockUserManager.Setup(x => x.GetRolesAsync(user)).ReturnsAsync(roles);
@@ -65,8 +67,6 @@ namespace FlightAPI.Tests.Services
 
             // Assert
             Assert.NotNull(result);
-            Assert.Equal(loginRequest.Email, result.Email);
-            _mockUserRepository.Verify(x => x.GetUserByEmail(loginRequest.Email), Times.Once);
             _mockUserManager.Verify(x => x.CheckPasswordAsync(user, loginRequest.Password), Times.Once);
         }
 
@@ -76,7 +76,7 @@ namespace FlightAPI.Tests.Services
             // Arrange
             var registerRequest = new RegisterRequestDTO
             {
-                Email = "jet@example.com",
+                Email = "jet2@example.com",
                 Password = "Boeing123@",
                 FirstName = "Test",
                 LastName = "User"
@@ -90,7 +90,7 @@ namespace FlightAPI.Tests.Services
                 NormalizedEmail = registerRequest.Email.ToUpper()
             };
 
-            _mockUserRepository.Setup(x => x.GetUserByEmail(registerRequest.Email)).ReturnsAsync((ApplicationUser)null);
+            _mockUserRepository.Setup(x => x.GetUserByEmail(registerRequest.Email, true)).ReturnsAsync((ApplicationUser)null);
             _mockUserManager.Setup(x => x.CreateAsync(It.IsAny<ApplicationUser>(), registerRequest.Password)).ReturnsAsync(IdentityResult.Success);
             _mockUserManager.Setup(x => x.AddToRoleAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Success);
 
@@ -98,8 +98,14 @@ namespace FlightAPI.Tests.Services
             await _authService.RegisterUser(registerRequest);
 
             // Assert
-            _mockUserRepository.Verify(x => x.GetUserByEmail(registerRequest.Email), Times.Once);
-            _mockUserManager.Verify(x => x.CreateAsync(It.Is<ApplicationUser>(u => u.Email == user.Email), registerRequest.Password), Times.Once);
+            _mockUserManager.Verify(x => x.CreateAsync(It.Is<ApplicationUser>(u =>
+                u.Email == registerRequest.Email &&
+                u.FirstName == registerRequest.FirstName &&
+                u.LastName == registerRequest.LastName &&
+                u.UserName == registerRequest.Email &&
+                u.NormalizedEmail == registerRequest.Email.ToUpper()),
+                registerRequest.Password), Times.Once);
+            _mockUserManager.Verify(x => x.AddToRoleAsync(It.Is<ApplicationUser>(u => u.Email == user.Email), It.IsAny<string>()), Times.Once);
         }
     }
 }
