bbd23d736e5bf69aed14c0fe512b2d22d0f975bf
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-05-29 21:13:00 +0200

    Add NullPlaneDataException

---
+++ FlightAPI/Exceptions/NullPlaneDataException.cs
@@ -0,0 +1,10 @@
+﻿namespace FlightAPI.Exceptions
+{
+    public class NullPlaneDataException : Exception
+    {
+        /// <summary>
+        /// Exception that is thrown when the plane entity is null.
+        /// </summary>
+        public NullPlaneDataException() : base("Plane data cannot be null") { }
+    }
+}
