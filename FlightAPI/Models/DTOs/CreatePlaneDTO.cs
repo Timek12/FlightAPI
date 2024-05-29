cce7b486c0726073bee1fc7a8f3eec558282fcde
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-05-29 21:13:30 +0200

    Add plane DTOs

--- FlightAPI/Models/DTOs/CreatePlaneDTO.cs
+++ FlightAPI/Models/DTOs/CreatePlaneDTO.cs
@@ -1,6 +1,14 @@
-﻿namespace FlightAPI.Models.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlightAPI.Models.DTOs
 {
     public record CreatePlaneDTO
     {
+        public string Model { get; set; }
+        public string Type { get; set; }
+        public string Manufacturer { get; set; }
+        public int Capacity { get; set; }
+        public double CruiseSpeed { get; set; }
+        public double Range { get; set; }
     }
 }
--- FlightAPI/Models/DTOs/UpdatePlaneDTO.cs
+++ FlightAPI/Models/DTOs/UpdatePlaneDTO.cs
@@ -2,5 +2,12 @@
 {
     public record UpdatePlaneDTO
     {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public string Type { get; set; }
+        public string Manufacturer { get; set; }
+        public int Capacity { get; set; }
+        public double CruiseSpeed { get; set; }
+        public double Range { get; set; }
     }
 }
